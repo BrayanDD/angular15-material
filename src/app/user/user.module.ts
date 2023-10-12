09333e071532d54d95e02b11d375acef7d10e397
BrayanDelgado <isosbrayan@gmail.com>
2023-10-11 21:03:55 -0500

    create package user, auth, page

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserDetailsComponent,
+    UserEditComponent,
+    UserCreateComponent,
+    UserListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
